ce5aa3bc56e0e13715c83e63ced5cf83d7785f12
natebenson32 <natebenson32@35.40.126.83>
2016-09-26 15:20:22 -0400

    First commit.

---
+++ src/cis350/Bye.java
@@ -0,0 +1,8 @@
+package cis350;
+
+public class Bye {
+
+	public static void main(String[] args) {
+		System.out.println("Bye world!");
+	}
+}
---
+++ src/cis350/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains classes used in demonstration of Git VCS.
+ * @author natebenson32
+ * @version 1.0
+ */
+package cis350;
