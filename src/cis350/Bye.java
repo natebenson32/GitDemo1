2dd3862008ec1cd9d96e93644a3992ab3601b304
natebenson32 <natebenson32@35.40.126.83>
2016-09-26 15:28:52 -0400

    Second commit.

--- src/cis350/Bye.java
+++ src/cis350/Bye.java
@@ -4,6 +4,5 @@ public class Bye {
 
 	public static void main(String[] args) {
 		Helper.display("Bye world!");
-		
 	}
 }
--- src/cis350/package-info.java
+++ src/cis350/package-info.java
@@ -3,5 +3,4 @@
  * @author natebenson32
  * @version 1.0
  */
-
 package cis350;
